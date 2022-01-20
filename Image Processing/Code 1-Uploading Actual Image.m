a1e992ce69d59cd564ade2ef76e2fd269f86375d
SriSaiSurya <93868932+SriSaiSurya@users.noreply.github.com>
2022-01-20 13:18:35 +0000

    Delete Image Processing directory

--- Image Processing/Code 1-Uploading Actual Image.m
+++
@@ -1,6 +0,0 @@
-load('original_image.jpg');
-imread('original_image.jpg');
-i =imread('original_image.jpg');    %Display Actual image
-imshow(i), figure(1)
-imfinfo ('original_image.jpg')
-whos picgard
