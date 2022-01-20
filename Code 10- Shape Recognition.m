7cf4dbd9a915856189a58eeb632aaa4dfd4fb251
SriSaiSurya <93868932+SriSaiSurya@users.noreply.github.com>
2022-01-20 15:40:09 +0000

    Delete Code 10- Shape Recognition.m

--- Code 10- Shape Recognition.m
+++
@@ -1,8 +0,0 @@
-function circleCyan(x,y,r,n)
-theta = linspace(0, 2*pi,n);
-x1 = x + r*sin(theta);
-y1 = y + r*cos(theta);
-plot(x,y, 'o', 'MarkerFaceColor', 'w', 'Markersize', 6), hold on
-plot(x1,y1, 'b-')
-h=fill(x1,y1,'c');
-axis equal
